0a3ad26e444c40d3b6b66e5f45eff15f5c4edb24
Citrio <jacob5263@jatcraft.com>
2019-05-16 23:24:18 -0500

    monkamega

--- Security Bot/Bot.cs
+++ Security Bot/Bot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Reflection.Metadata;
 using System.Threading.Tasks;
@@ -47,10 +48,47 @@ namespace Security_Bot
 
 		public async Task HandleCommands(CommandContext context)
 		{
+			if (context.Message.Content.ToLower().StartsWith(program.config.BotPrefix + "report") && ((IGuildUser) context.Message.Author).RoleIds.Any(p => program.config.ReportRoleIDs.Contains(p.ToString())))
+			{
+				DoReportCommand(context);
+			}
 			if (context.Message.Content.ToLower().StartsWith(program.config.BotPrefix + "ban") && ((IGuildUser) context.Message.Author).RoleIds.Any(p => program.config.ReportRoleIDs.Contains(p.ToString())))
 			{
-				
+				BanSystem.DoDiscordBanCommand(context);
 			}
+			if (context.Message.Content.ToLower().StartsWith(program.config.BotPrefix + "reason") && ((IGuildUser) context.Message.Author).RoleIds.Any(p => program.config.ReportRoleIDs.Contains(p.ToString())))
+			{
+				BanSystem.DoReasonCmd(context);
+			}
+			if (context.Message.Content.ToLower().StartsWith(program.config.BotPrefix + "baninfo") && ((IGuildUser) context.Message.Author).RoleIds.Any(p => program.config.ReportRoleIDs.Contains(p.ToString())))
+			{
+				BanSystem.DoGetInfoCommand(context);
+			}
+			if (context.Message.Content.ToLower().StartsWith(program.config.BotPrefix + "unban") && ((IGuildUser) context.Message.Author).RoleIds.Any(p => program.config.ReportRoleIDs.Contains(p.ToString())))
+			{
+				BanSystem.DoUnbanCmd(context);
+			}
+		}
+
+		public async Task DoReportCommand(CommandContext context)
+		{
+			string[] args = context.Message.Content.Split(new string[] {" "}, StringSplitOptions.None);
+			if (args.Length < 3)
+			{
+				await context.Channel.SendMessageAsync(
+					$"Invalid arguments. Usage: {program.config.BotPrefix}ban (SteamID64) (proof link) (reason)");
+				return;
+			}
+
+			if (!ulong.TryParse(args[1], out ulong steamid))
+			{
+				await context.Channel.SendMessageAsync(
+					$"Invalid SteamID64. Usage: {program.config.BotPrefix}ban (SteamID64) (proof link) (reason)");
+				return;
+			}
+
+			string[] reasonArray = args.Where(p => p != args[0] && p != args[1] && p != args[2]).ToArray();
+			string reason = string.Join(' ', reasonArray);
 		}
 	}
 }
--- Security Bot/ReportHandler.cs
+++ Security Bot/ReportHandler.cs
@@ -1,19 +1,40 @@
+using System.Collections.Specialized;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
 
 namespace Security_Bot
 {
 	public class ReportHandler
 	{
 		private Program program;
+		private WebClient client;
+		public const string kReportURL = "https://titnoas.xyz/PepeSmug/reportcheatertest.php";
 
 		public ReportHandler(Program program)
 		{
 			this.program = program;
+			client = new WebClient();
 		}
 
-		public async Task ReportCheater(string SteamID64, string reason, string proof)
+		public async Task<CheaterReportResponse> ReportCheater(string SteamID64, string reason, string proof)
 		{
-			
+			NameValueCollection collection = new NameValueCollection();
+			collection.Add("serverhostkey", program.config.ReportKey);
+			collection.Add("cheatdescription", reason);
+			collection.Add("serverhostprooflink", proof);
+			collection.Add("serverhoststeamid", SteamID64);
+			byte[] response = await client.UploadValuesTaskAsync(kReportURL, collection);
+			return JsonConvert.DeserializeObject<CheaterReportResponse>(Encoding.UTF8.GetString(response));
 		}
+	}
+
+	public class CheaterReportResponse
+	{
+		public string message { get; set; }
+		public string error { get; set; }
 	}
 }
